2afa542258c833343d9b12c1ca8d7c27d36f971b
John Peterson <jlpeterson829@gmail.com>
2015-02-13 19:22:20 -0800

    Move enemies to new random locations every 2 seconds

--- watchout.js
+++ watchout.js
@@ -14,6 +14,7 @@ var axes = {
   x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
 }
+
 var createEnemies = function() {
   var enemyData = [];
   for (var i = 0; i < gameOptions.nEnemies; i++) {
@@ -33,64 +34,29 @@ var gameboard = d3.select('body').append('svg')
 
 var enemyPositions = createEnemies(); 
 
-var circles = gameboard.selectAll('circle')
+var enemies = gameboard.selectAll('circle')
                 .data(enemyPositions)
                 .enter()
                 .append('circle');
 
 
-circles.attr("cx",function(d,i){return enemyPositions[i].x})
+enemies.attr("cx",function(d,i){return enemyPositions[i].x})
         .attr("cy", function(d,i){return enemyPositions[i].y})
         .attr("r", 5)
         .style("fill", "black");
 
+var gameTurn = function() {
+  var newPositions = createEnemies();
+
+  gameboard.selectAll('circle')
+            .data(newPositions)
+            .attr("cx", function(d, i){return newPositions[i].x})
+            .attr("cy", function(d, i){return newPositions[i].y})
+            .transition()
+            .duration(2000);
+}
+
+gameTurn();
+setInterval(gameTurn, 2000);
 
 
-// var circles = svg.selectAll('circle')
-//                 .data()
-//                 .enter()
-//                 .append('circle');
-
-
-// var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
-// var width = 960,
-//     height = 500;
-
-// var svg = d3.select('body').append('svg')
-//         .attr('width', width)
-//         .attr('height', height)
-//         .append('g')
-//         .attr('transform', 'translate(32, ' + (height / 2) + ')');
-
-// function update(data) {
-//   var text = svg.selectAll('text').data(data);
-
-//   text.attr('class', 'enter');
-
-//   text.enter().append('text')
-//       .attr('class', 'enter')
-//       .attr('x', function(d, i) {return i * 32;})
-//       .attr('dy', '.35em');
-
-//   text.text(function(d) { return d; });
-
-//   text.exit().remove();
-// }
-
-// update(alphabet);
-
-// setInterval(function() {
-//   update(shuffle(alphabet)
-//     .slice(0, Math.floor(Math.random() * 26))
-//     .sort());
-// }, 1500);
-
-// function shuffle(array) {
-//   var m = array.length, t, i;
-//   while (m) {
-//     i = Math.floor(Math.random() * m--);
-//     t = array[m], array[m] = array[i], array[i] = t;
-//   }
-//   return array;
-// }
