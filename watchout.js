b76a46e4f75955493bac9983d0a905afcf74c3db
John Peterson <jlpeterson829@gmail.com>
2015-02-14 11:53:42 -0800

    Add player, but when clicked, shoots to top left corner

--- watchout.js
+++ watchout.js
@@ -28,35 +28,69 @@ var createEnemies = function() {
   return enemyData;
 };  
 
+var createPlayer = function(){
+  var playerData = [];
+  var player = {};
+  player.x = gameOptions.width/2;
+  player.y = gameOptions.height/2;
+  playerData.push(player);
+  return playerData;
+};
+
+var dragCircle = d3.behavior.drag()
+    .on('drag', function (d, i) {
+    d.cx += d3.event.dx;
+    d.cy += d3.event.dy;
+    d3.select('circle.player').attr('cx', d.cx).attr('cy', d.cy);
+});
+
 var gameboard = d3.select('body').append('svg')
                   .attr('width', gameOptions.width)
                   .attr('height', gameOptions.height);
+                  
 
+var originalPlayer = createPlayer();
+//debugger;
+
+var player = gameboard.selectAll('circle')
+                .data(originalPlayer)
+                .enter()
+                .append('circle')
+                .attr("class", "player")
+                .attr("cx", function(d){return d.x;})
+                .attr("cy", function(d){return d.y;})
+                .attr('r', 10)
+                .style("fill", "red")
+                .call(dragCircle);
+//debugger;
 var enemyPositions = createEnemies(); 
 
 var enemies = gameboard.selectAll('circle')
                 .data(enemyPositions)
                 .enter()
                 .append('circle')
+                .attr("class", "enemy")
                 .attr("cx", function(d,i){return enemyPositions[i].x})
                 .attr("cy", function(d,i){return enemyPositions[i].y})
                 .attr("r", 5)
                 .style("fill", "black");
 
 
+
 var gameTurn = function() {
   var newPositions = createEnemies();
 
-  gameboard.selectAll('circle')
+  gameboard.selectAll('circle.enemy')
             .data(newPositions)
             .transition()
             .attr("cx", function(d, i){return newPositions[i].x})
             .attr("cy", function(d, i){return newPositions[i].y})
             .duration(2000);
-}
+};
 
 gameTurn();
-setInterval(gameTurn, 1000);
+setInterval(gameTurn, 2000);
+
 
 
 
